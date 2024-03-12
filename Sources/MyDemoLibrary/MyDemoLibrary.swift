9fbff0e17d498a634bae4ae647e66583b3e4f232
BizBrolly <bizbrolly@BizBrollys-MacBook-Air-3.local>
2024-03-12 17:52:29 +0530

    corner radius

--- Sources/MyDemoLibrary/MyDemoLibrary.swift
+++ Sources/MyDemoLibrary/MyDemoLibrary.swift
@@ -1,11 +1,28 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-import Foundation
+import UIKit
 public class JSONParser {
 
   public static func parse<T: Decodable>(data: Data) throws -> T {
     let decoder = JSONDecoder()
     return try decoder.decode(T.self, from: data)
   }
+}
+
+public class MakeRoundedCorder: UIView{
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.roundedCorner()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func roundedCorner(){
+        self.layer.cornerRadius = 10
+        self.layer.backgroundColor = UIColor.green.cgColor
+        self.layer.masksToBounds = true
+    }
 }
